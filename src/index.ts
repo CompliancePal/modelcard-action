10c22f0b1f472b4a029386a71e33b1d23ef9f751
Markus Holopainen <markusp_holopainen@yahoo.com>
2022-06-10 15:41:32 +0300

    Changed path construction from template strings to using the 'path' library

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,16 @@ import * as yaml from 'js-yaml';
 import * as core from '@actions/core';
 import { RulesetDefinition } from '@stoplight/spectral-core'
 import { validator } from './validator';
+import path from 'path';
 
 const MLPROJECT_PATH: string = './MLproject';
 const ROOT_PATH: string = process.env.GITHUB_WORKSPACE || process.cwd()
 
-const load_custom_ruleset = (path: string | null): RulesetDefinition | undefined => {
-  if (path) {
+const load_custom_ruleset = (rulePathRelative: string | null): RulesetDefinition | undefined => {
+  if (rulePathRelative) {
     try {
-      const rule_dir_path = `${ROOT_PATH}/${path}`
-      const custom_rules: RulesetDefinition = require(`${rule_dir_path}/rules`)(rule_dir_path);
+      const rulePathAbs = path.join(ROOT_PATH, rulePathRelative)
+      const custom_rules: RulesetDefinition = require(path.join(rulePathAbs, 'rules'))(rulePathAbs);
       return custom_rules;
     } catch (err) {
       console.log(err);
