d9317a9163c6a7dc0340caa6318b9ab99ea1708d
Markus Holopainen <markusp_holopainen@yahoo.com>
2022-06-10 12:00:00 +0300

    Added possibility to include custom rules

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
 import * as fs from 'fs';
 import * as yaml from 'js-yaml';
 import * as core from '@actions/core';
+import { RulesetDefinition } from '@stoplight/spectral-core'
 import { validator } from './validator';
 
 const MLPROJECT_PATH: string = './MLproject';
+const ROOT_PATH: string = process.env.GITHUB_WORKSPACE || process.cwd()
+
+const load_custom_ruleset = (path: string | null): RulesetDefinition | undefined => {
+  if (path) {
+    try {
+      const rule_dir_path = `${ROOT_PATH}/${path}`
+      const custom_rules: RulesetDefinition = require(`${rule_dir_path}/rules`)(rule_dir_path);
+      return custom_rules;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  return undefined;
+}
 
 const main = async () => {
   try {
@@ -15,9 +30,9 @@ const main = async () => {
 
     const mlproject: any = yaml.load(fs.readFileSync(MLPROJECT_PATH, 'utf8'));
     console.log('MLproject file opened');
-    const modelcard_path: string = mlproject.modelcard;
 
     //Check that modelcard is defined in the MLproject file
+    const modelcard_path: string = mlproject.modelcard;
     if (!modelcard_path) {
       throw new Error("'modelcard' property not found in MLproject file!");
     }
@@ -26,10 +41,12 @@ const main = async () => {
       const raw = fs.readFileSync(modelcard_path, 'utf8');
       console.log('Model card file opened');
 
-      // Do stuff with the model card file
+      //Use custom ruleset if one is defined
+      const custom_ruleset_path: string = mlproject.modelcard_rules;
+      const custom_rules: RulesetDefinition | undefined = load_custom_ruleset(custom_ruleset_path);
 
       // Find problems
-      const diagnostics = await validator(raw);
+      const diagnostics = await validator(raw, custom_rules);
 
       // TODO: iterate over diagnostics
 
@@ -37,6 +54,7 @@ const main = async () => {
         console.log(problem);
       });
     } catch (e) {
+      console.log(e);
       throw new Error('Could not open model card file!');
     }
   } catch (error) {
--- src/validator/index.ts
+++ src/validator/index.ts
@@ -1,11 +1,16 @@
-import { Spectral, Document } from '@stoplight/spectral-core';
+import { Spectral, Document, RulesetDefinition } from '@stoplight/spectral-core';
 import * as Parsers from '@stoplight/spectral-parsers';
 import rules from './rules';
 
-export const validator = async (content: string) => {
+export const validator = async (content: string, customRules?: RulesetDefinition) => {
   const spectral = new Spectral();
-
-  spectral.setRuleset(rules);
+  if (customRules){
+    const extendedRules: RulesetDefinition = { ...customRules, extends: rules };
+    spectral.setRuleset(extendedRules);
+  } else {
+    spectral.setRuleset(rules);
+  }
+  
 
   const modelCard = new Document(content, Parsers.Yaml);
 
