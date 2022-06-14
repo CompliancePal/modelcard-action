49a57f1c3f2549d8927639395a0d271f2fe83b82
Vlad Știrbu <vstirbu@gmail.com>
2022-06-14 14:26:15 +0300

    test check run report

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,30 @@
 import * as fs from 'fs';
 import * as yaml from 'js-yaml';
 import * as core from '@actions/core';
-import { RulesetDefinition } from '@stoplight/spectral-core'
+import * as github from '@actions/github';
+import { RulesetDefinition } from '@stoplight/spectral-core';
 import { validator } from './validator';
 import path from 'path';
 import 'dotenv/config';
 
-const ROOT_PATH: string = process.env.GITHUB_WORKSPACE || process.cwd()
+const ROOT_PATH: string = process.env.GITHUB_WORKSPACE || process.cwd();
 
 const load_custom_ruleset = (): RulesetDefinition | undefined => {
   const rulePathRelative = process.env.INPUT_RULES;
   if (rulePathRelative) {
     try {
       const rulePathAbs = path.join(ROOT_PATH, rulePathRelative);
-      const custom_rules: RulesetDefinition = require(path.join(rulePathAbs, 'rules'))(rulePathAbs);
+      const custom_rules: RulesetDefinition = require(path.join(
+        rulePathAbs,
+        'rules',
+      ))(rulePathAbs);
       return custom_rules;
     } catch (err) {
       console.log(err);
     }
   }
   return undefined;
-}
+};
 
 const main = async () => {
   if (!process.env.INPUT_MODELCARD) {
@@ -38,8 +42,25 @@ const main = async () => {
 
   // TODO: iterate over diagnostics
 
-  diagnostics.forEach((problem) => {
-    console.log(problem);
+  const octokit = github.getOctokit(core.getInput('token'));
+
+  const summary = diagnostics
+    .map((problem) => {
+      console.log(problem);
+      return `- \`${problem.path.join('.')}\``;
+    })
+    .join('\n');
+
+  await octokit.request('POST /repos/{owner}/{repo}/check-runs', {
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    head_sha: github.context.sha,
+    name: 'mlops/ModelCard validation',
+    conclusion: diagnostics.length > 0 ? 'failure' : 'success',
+    output: {
+      title: 'Validation problems',
+      summary,
+    },
   });
 };
 
