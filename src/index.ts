5e27f4655476d549f97389dc7f6ff6137af3190d
Markus Holopainen <markusp_holopainen@yahoo.com>
2022-08-10 14:38:24 +0300

    Now loads YAML ruleset

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,29 @@
 import * as fs from 'fs';
 import * as core from '@actions/core';
-import { RulesetDefinition } from '@stoplight/spectral-core';
+import { RulesetDefinition, Ruleset } from '@stoplight/spectral-core';
+import { bundleAndLoadRuleset } from '@stoplight/spectral-ruleset-bundler/dist/loader/node';
+import * as spectralRuntime from '@stoplight/spectral-runtime';
 import { validator } from './validator';
 import path from 'path';
 import 'dotenv/config';
 import { makeCheckRun } from './helpers/check';
 
+const { fetch } = spectralRuntime;
+
 const ROOT_PATH: string = process.env.GITHUB_WORKSPACE || process.cwd();
 
-const load_custom_ruleset = (): RulesetDefinition | undefined => {
+const load_custom_ruleset = async (): Promise<
+  RulesetDefinition | undefined
+> => {
   const rulePathRelative = process.env.INPUT_RULES;
   if (rulePathRelative) {
     try {
-      const rulePathAbs = path.join(ROOT_PATH, rulePathRelative);
-      const custom_rules: RulesetDefinition = require(path.join(
-        rulePathAbs,
-        'rules',
-      ))(rulePathAbs);
-      return custom_rules;
+      const rulePathAbs = path.join(ROOT_PATH, rulePathRelative, 'rules.yaml');
+      const custom_rules: Ruleset = await bundleAndLoadRuleset(rulePathAbs, {
+        fs,
+        fetch,
+      });
+      return custom_rules.definition;
     } catch (err) {
       console.log(err);
     }
@@ -36,7 +42,8 @@ const main = async () => {
   core.info('Model card file opened');
 
   //Use custom ruleset if one is defined
-  const custom_rules: RulesetDefinition | undefined = load_custom_ruleset();
+  const custom_rules: RulesetDefinition | undefined =
+    await load_custom_ruleset();
 
   // Find problems
   const diagnostics = await validator(raw, custom_rules);
--- src/validator/index.ts
+++ src/validator/index.ts
@@ -1,21 +1,31 @@
-import { Spectral, Document, RulesetDefinition } from '@stoplight/spectral-core';
+import {
+  Spectral,
+  Document,
+  RulesetDefinition,
+} from '@stoplight/spectral-core';
 import * as Parsers from '@stoplight/spectral-parsers';
 import rules from './rules';
 
-export const validator = async (content: string, customRules?: RulesetDefinition) => {
+export const validator = async (
+  content: string,
+  customRules?: RulesetDefinition,
+) => {
   const spectral = new Spectral();
-  const disable_default: boolean = process.env.INPUT_DISABLE_DEFAULT_RULES?.toLowerCase() === 'true';
-  if (customRules){
+  const disable_default: boolean =
+    process.env.INPUT_DISABLE_DEFAULT_RULES?.toLowerCase() === 'true';
+  if (customRules) {
     if (disable_default) {
       spectral.setRuleset(customRules);
     } else {
-      const extendedRules: RulesetDefinition = { ...customRules, extends: rules };
+      const extendedRules: RulesetDefinition = {
+        ...customRules,
+        extends: rules,
+      };
       spectral.setRuleset(extendedRules);
     }
   } else {
     spectral.setRuleset(rules);
   }
-  
 
   const modelCard = new Document(content, Parsers.Yaml);
 
