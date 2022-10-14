9b1320c36e24e399a9cdebe10c357b88b259b9b1
Vlad Știrbu <vstirbu@gmail.com>
2022-10-14 17:33:45 +0300

    load schema

--- packages/action/src/validator/rules.ts
+++ packages/action/src/validator/rules.ts
@@ -1,5 +1,15 @@
 import { RulesetDefinition } from '@stoplight/spectral-core';
 import { schema } from '@stoplight/spectral-functions';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+const loadSchema = () =>
+  JSON.parse(
+    readFileSync(
+      join(__dirname, '../../resources/schemas/modelcard-v0.0.2.json'),
+      'utf8',
+    ),
+  );
 
 const rules: RulesetDefinition = {
   rules: {
@@ -8,7 +18,7 @@ const rules: RulesetDefinition = {
       then: {
         function: schema,
         functionOptions: {
-          schema: require('../../resources/schemas/modelcard-v0.0.2.json'),
+          schema: loadSchema(),
           dialect: 'draft7',
         },
       },
