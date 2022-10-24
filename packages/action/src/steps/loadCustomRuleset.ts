df99b9866cd51ca2aa11d44780dfa716beafb037
Vlad Știrbu <vstirbu@gmail.com>
2022-10-24 15:39:02 +0300

    name

--- packages/action/src/steps/loadCustomRuleset.ts
+++ packages/action/src/steps/loadCustomRuleset.ts
@@ -50,7 +50,8 @@ export const loadCustomRuleset = async (): Promise<
             owner: github.context.repo.owner,
             repo: github.context.repo.repo,
             head_sha: github.context.sha,
-            name: 'modelcard/ruleset-validation',
+            // name: 'modelcard/ruleset-validation',
+            name: process.env.GITHUB_JOB || 'modelcard/ruleset-validation',
             conclusion: 'failure',
             output: {
               title: 'Validation problems',
