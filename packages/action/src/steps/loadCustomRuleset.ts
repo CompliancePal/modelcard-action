8150bcb65e7bd8585b7154b751ef160211e16a75
Vlad Știrbu <vstirbu@gmail.com>
2022-10-24 18:53:09 +0300

    annotation from logs

--- packages/action/src/steps/loadCustomRuleset.ts
+++ packages/action/src/steps/loadCustomRuleset.ts
@@ -1,14 +1,14 @@
 import path from 'path';
 import * as core from '@actions/core';
-import * as github from '@actions/github';
+// import * as github from '@actions/github';
 import {
   loader,
   RulesetValidationError,
 } from '@compliancepal/spectral-rulesets';
 import { RulesetDefinition } from '@stoplight/spectral-core';
-import { renderRulesetValidationSummary } from '../helpers/templates';
-import getOctokit from '../helpers/octokit';
-import { CHECK_NAME } from '../helpers/check';
+// import { renderRulesetValidationSummary } from '../helpers/templates';
+// import getOctokit from '../helpers/octokit';
+// import { CHECK_NAME } from '../helpers/check';
 
 export const loadCustomRuleset = async (): Promise<
   RulesetDefinition | undefined
@@ -28,48 +28,53 @@ export const loadCustomRuleset = async (): Promise<
       core.info(`problems in file ${filepath}`);
 
       error.annotations.forEach((annotation) => {
-        core.info(`${annotation.jsonPath.join('.')} - ${annotation.message}`);
+        core.error(`${annotation.jsonPath.join('.')} - ${annotation.message}`, {
+          file: filepath,
+          title: annotation.title,
+          startLine: annotation.start_line,
+          endLine: annotation.end_line,
+        });
       });
 
-      try {
-        const octokit = getOctokit();
+      // try {
+      //   const octokit = getOctokit();
 
-        if (!octokit) {
-          core.setFailed('GitHub TOKEN required');
+      //   if (!octokit) {
+      //     core.setFailed('GitHub TOKEN required');
 
-          return;
-        }
+      //     return;
+      //   }
 
-        const annotations = error.annotations.map(
-          ({ start_column, end_column, title, ...rest }) => ({
-            ...rest,
-            path: filepath,
-          }),
-        );
+      //   const annotations = error.annotations.map(
+      //     ({ start_column, end_column, title, ...rest }) => ({
+      //       ...rest,
+      //       path: filepath,
+      //     }),
+      //   );
 
-        const response = await octokit.request(
-          'POST /repos/{owner}/{repo}/check-runs',
-          {
-            owner: github.context.repo.owner,
-            repo: github.context.repo.repo,
-            head_sha: github.context.sha,
-            name: CHECK_NAME,
-            conclusion: 'failure',
-            output: {
-              title: 'Validation problems',
-              summary: renderRulesetValidationSummary({ annotations }),
-              annotations,
-            },
-            external_id: `action-${process.env.GITHUB_RUN_ID}`,
-          },
-        );
+      //   const response = await octokit.request(
+      //     'POST /repos/{owner}/{repo}/check-runs',
+      //     {
+      //       owner: github.context.repo.owner,
+      //       repo: github.context.repo.repo,
+      //       head_sha: github.context.sha,
+      //       name: CHECK_NAME,
+      //       conclusion: 'failure',
+      //       output: {
+      //         title: 'Validation problems',
+      //         summary: renderRulesetValidationSummary({ annotations }),
+      //         annotations,
+      //       },
+      //       external_id: `action-${process.env.GITHUB_RUN_ID}`,
+      //     },
+      //   );
 
-        core.info(
-          `Created a check run https://github.com/${github.context.repo.owner}/${github.context.repo.repo}/runs/${response.data.id}`,
-        );
-      } catch (e) {
-        console.log(e);
-      }
+      //   core.info(
+      //     `Created a check run https://github.com/${github.context.repo.owner}/${github.context.repo.repo}/runs/${response.data.id}`,
+      //   );
+      // } catch (e) {
+      //   console.log(e);
+      // }
     }
 
     if (error instanceof Error) {
