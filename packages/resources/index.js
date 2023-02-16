ce71a8cc7631ee8028c7e0448388dfc139eca3f7
Vlad Știrbu <vstirbu@gmail.com>
2023-02-16 18:18:16 +0100

    basic nextjs app example

---
+++ packages/resources/index.js
@@ -0,0 +1,5 @@
+const schema = require('./modelcard-v0.0.2.json');
+
+module.exports = {
+  schema,
+};
--- packages/rulesets/src/rules.ts
+++ packages/rulesets/src/rules.ts
@@ -2,8 +2,10 @@ import { RulesetDefinition } from '@stoplight/spectral-core';
 import { schema } from '@stoplight/spectral-functions';
 import { readFileSync } from 'fs';
 import { join } from 'path';
+// @ts-ignore
+import { schema as modelCardSchema } from 'resources';
 
-const loadSchema = () =>
+export const loadSchema = () =>
   JSON.parse(
     readFileSync(
       join(__dirname, '../resources/schemas/modelcard-v0.0.2.json'),
@@ -18,7 +20,7 @@ export const defaultRules: RulesetDefinition = {
       then: {
         function: schema,
         functionOptions: {
-          schema: loadSchema(),
+          schema: modelCardSchema,
           dialect: 'draft7',
         },
       },
